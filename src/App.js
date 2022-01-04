1fa21754635784ec4c1874272b43387a005aee6c
YosstinCode <Yosstincode@gmail.com>
2022-01-04 11:51:44 -0500

    structure initial to weather-app, webpack config for react

---
+++ src/App.js
@@ -0,0 +1,3 @@
+export default function App() {
+    return (<h1 className="container">Hello world!</h1>)
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const rulesForStyle = {
+  test: /\.css$/,
+  use: ["style-loader", "css-loader"], // style-loader: cuando encuentra el import en el js lee el style
+  //css-loader: cuando termine el loader anterior resuelve los import y require de los archivos del css
+};
+
+const rulesForJS = {
+  test: /\.(js|jsx)$/, // aqui indicamos los archivos que webpack no entiende
+  loader: "babel-loader", //le indicamos porque cargador debe pasar
+  options: {
+    //aqui indicamos una configuracion de babel para react.
+    presets: [
+      [
+        //le pasamos un array para poder configurar el preset
+        "@babel/preset-react",
+        {
+          runtime: "automatic", //classic
+        },
+      ], //npm i @babel/core babel-loader @babel/preset-react --save-dev
+    ],
+  },
+};
+
+const rules = [rulesForJS, rulesForStyle];
+
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const isProduction = mode === "production";
+
+  return {
+    //aqui configuramos donde sera nuestra carpeta de build
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "main.js",
+      path: path.resolve(__dirname, "build"),
+    },
+    //aqui agregamos los plugin los plugins le agregan a webpack una nueva funcionalidad.
+    plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+    //aqui configuramos los loaders
+    module: {
+      rules,
+    },
+    //config server webpack
+    devServer: {
+      open: true, // abrir navegador
+      port: 3000, //puerto
+      compress: true, //ni idea se supone que gzip
+    },
+  };
+};
